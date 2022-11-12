de55663e0173bc66260d330f2ffab8fcd8d2098f
pzagmel <pedro.zagalm@gmail.com>
2022-11-12 16:11:47 +0000

    second commit

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -4,8 +4,8 @@ const Card = ({img, title, descripcion, textbutton}) => {
 
   return (
     <div className="card ">
-      <img src={img} className="card-img-top" alt="..." />
-      <div className="card-body">
+      <img src={img} className="card-img-top" style={{"width":"305px","height":"200px"}} />
+      <div className="card-body" style={{"height":"230px"}}>
         <h5 className="card-title">{title}</h5>
         <p className="card-text">
          {descripcion}
--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -6,31 +6,47 @@ import Footer from "./Footer.jsx";
 
 //create your first component
 const Home = () => {
-  const data = [
-    "http://greenarea.me/wp-content/uploads/2016/02/img.rtve_-500x325.jpg",
-    "https://images.placesonline.com/photos/53749_bari_porto_di_bari.jpg?quality=80&w=710&h=510&mode=crop",
-    "http://4.bp.blogspot.com/-1Lp5wkYQqYk/TvuCF0_u7jI/AAAAAAAAAdE/aWg6u7lIfbE/s1600/Galletas-Caballos-500x325.jpg",
-    "https://mejoresseries.org/wp-content/uploads/2021/01/medici-serie-italiana-500x325.jpg",
-  ];
+  const datos = [
+     {
+             title:"Abierto de Australia",
+             img:"https://e00-marca.uecdn.es/assets/multimedia/imagenes/2021/02/12/16131067902692.jpg",
+             descripcion:"Se juega a primeros de año en Sidney sobre pista dura. Es un torneo muy exigente en el que calor juega un papel protagonista.",
+             textbutton:"Find Out More!"
+            },
+     {
+             title:"Roland Garros",
+             img:"https://assets.adnradio.cl/2022/08/Clipboard-2-925x470.jpg",
+             descripcion:"Es el campeonato más importante sobre pista de tierra. Tiene lugar en entre el mes de mayo y junio en París. Si un jugador es protagonista en este torneo ese es Rafa Nadal, que ha conseguido alzarse 11 victorias sobre la arcilla de París.",
+             textbutton:"Find Out More!"
+            },
+     {
+             title:"Wimbledon",
+             img:"https://content.rolex.com/dam/world-of-rolex/tennis/wimbledon/2022/share-search/rolex-and-tennis-wimbledon-2022-share_wim21jb_01153_r.jpg",
+             descripcion:"Se disputa en el mes de julio en la localidad inglesa de Wimbledon. Se juega sobre hierba. Los competidores están obligados a vestir de blanco durante los partidos.",
+             textbutton:"Find Out More!"
+            },
+     {
+             title:"US Open",
+             img:"https://www.record.com.mx/sites/default/files/styles/amp-discovery-1200x/public/articulos/2021/08/30/us_open_2.jpg?itok=tkyx2yBW",
+             descripcion:"Tiene lugar en el mes de octubre en New York. Se juega en pistas duras. Es muy común ver a estrellas internacionales de cine animando a sus jugadores favoritos desde la gradas.",
+             textbutton:"Find Out More!"
+            },
+    ];
   return (
     <>
       <Navbar />
       <div className="container">
         <Jumbotron
-          title="A Warm Welcome!"
-          descripcion="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+          title="Te invitamos a conocer los 4 Grand Slam!"
+          descripcion="El Grand Slam de tenis está constituido por los cuatro torneos oficiales mayores del circuito internacional organizados por la Federación Internacional de Tenis."
           textbutton="Call to action!"
         />
         <div className="row row-cols-1 row-cols-md-4 g-4">
-          {data.map((item) => {
+          {datos.map((objeto,posicion) => {
             return (
               <div className="col text-center">
-                <Card
-                  img={item}
-                  title="Card title"
-                  descripcion="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                  textbutton="Find Out More!"
-                />
+                <Card title={objeto.title} img={objeto.img} descripcion={objeto.descripcion} textbutton={objeto.textbutton}/>
+                
               </div>
             );
           })}
